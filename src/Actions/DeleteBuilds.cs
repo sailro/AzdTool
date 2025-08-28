3fd40c40881f3ba066cc7185221fd8a6134b6c0a
Sebastien Lebreton <sebastien@lebreton.fr>
2025-08-28 14:58:15 +0200

    Add delete build(s) command

---
+++ src/Actions/DeleteBuilds.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using AzdTool.Extensions;
+using AzdTool.Visitors;
+using Microsoft.TeamFoundation.Build.WebApi;
+using AZBuild = Microsoft.TeamFoundation.Build.WebApi.Build;
+
+namespace AzdTool.Actions;
+
+internal class DeleteBuilds(VisitorNode node) : BatchOrUnitAction<AZBuild>(node, "build", "delete")
+{
+	protected override bool Filter(AZBuild item, string filter) => Regex.IsMatch(item.GetFormattedTitle(), filter, RegexOptions.IgnoreCase);
+
+	protected override async Task ActionAsync(IEnumerable<AZBuild> items)
+	{
+		var organization = Node.Ancestor<Organization>();
+		await DeleteBuildsAsync(organization, items);
+	}
+
+	public static async Task DeleteBuildsAsync(Organization organization, IEnumerable<AZBuild> items)
+	{
+		await organization.ExecuteClientAsync<BuildHttpClient>(async client =>
+		{
+			foreach (var item in items)
+			{
+				var leases = await client.GetRetentionLeasesForBuildAsync(item.Project.Id, item.Id);
+				if (leases.Count > 0)
+				{
+					await client.DeleteRetentionLeasesByIdAsync(item.Project.Id, leases.Select(l => l.LeaseId));
+				}
+
+				await client.DeleteBuildAsync(item.Project.Id, item.Id);
+			}
+		});
+	}
+}
